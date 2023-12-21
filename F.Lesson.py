cef5209e40d0de0d34f3863475f8ab46e4043ff7
opilane <opilane@HP-CZC2349HTM>
2023-12-21 12:35:08 +0200

    21.12.23

--- F.Lesson.py
+++ F.Lesson.py
@@ -1,14 +1,36 @@
+#1 ÜL
+#print("Tere Maailm!")
+#nimi=input("Mis on sinu nimi?")
+#vanus=int(input("Kui vana sa oled")) 
+#print("Tere tulemast!"+ " "+ nimi + ". Sa oled"+str(vanus)+"aastat vana")
+#print("Tere tulemast!", nimi, ". Sa oled",vanus,"aastat vana")
+#print("Tere tulemast! {0} Sa oled {1} aastat vana".format(nimi, vanus)) 
+#print("Muutuja vanus=",vanus,"tüüp on",type(vanus))
 
-print("Tere Maailm!")
-print("Tere Maailm!")
-print("Tere Maailm!")
-print("Tere Maailm!")
-print("Tere Maailm!")
-print("Tere Maailm!")
-print("Tere Maailm!")
-print("Tere Maailm!")
 
-print("Tere Maailm!")
-print("Tere Maailm!")
-print("Tere Maailm!")
-print("Tere Maailm!")
+
+#2 ÜL
+#vanus = 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#kas_käib_koolis = True
+
+#print("Muutuja vanus=",type(vanus))
+#print("Muutuja eesnimi=",type(eesnimi))
+#print("Muutuja pikkus=",type(pikkus))
+#print("Muutuja kas_käib_koolis=",type(kas_käib_koolis))
+
+
+
+#3 ÜL
+#kommid = 500
+#print(kommid)
+#mitu_kommid_võtta = int(input("Mitu kommi sa tahad ära võtta?"))
+#kommid -= mitu_kommid_võtta
+#print(kommid)
+ 
+
+
+#4 ÜL
+puu_ümbermõõt = int(input("Milline on puu ümbermõõt?"))
+print("puu läbimõõt on võrdne", 2 * puu_ümbermõõt)
